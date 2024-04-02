d995ad97ed8adf4b3ca90f06c17c64e3c83f5ca6
pablovanegas <104100454+pablovanegas@users.noreply.github.com>
2024-04-02 16:12:07 -0500

    Design 3

--- ui.R
+++ ui.R
@@ -1,62 +1,74 @@
-# ui.R
-library(shinyAce)
 library(shiny)
-library(markdown)
-library(knitr)
-library(dplyr)
-library(shinyjs)
-library(shinythemes)  
-# Design 2: 
-shinyUI(
-  # sidebar hidden
-  tagList(
-    tags$div(
-      fluidPage(
-        useShinyjs(),  
-        div(id = "sidebar",
-            sidebarPanel(
-              h2("Opciones"),
-              actionButton("theme_button", "Elige un tema"),  
-              selectInput('theme_code', 'Tema editor', choices = themes, selected = 'ambiance'),
-              downloadButton('save_code', 'Guardar codigo', icon = icon('save')),
-              downloadButton('save_knit', 'Guardar knitr', icon = icon('save')),
-            )
-        ),
-          # Comienzo main panel
-          mainPanel(
-            actionButton("toggleSidebar", "Opciones"),
-            h1("Shiny Ace knitr Example"),
-            tags$div(
-              class = "row",
-              tags$div(
-                class = "col-md-4",
-                h2("Source R-Markdown"),
-                verbatimTextOutput("log"),
-                aceEditor("rmd", mode = "markdown", value = init,
-                          hotkeys = list(
-                            open_chunk = 'Ctrl-Alt-I',
-                            save_code = "Ctrl-S",
-                            help_key = "F1"
-                          ),
-                          autoComplete = "live"),
-                actionButton("eval", "Run", icon = icon('play')),
-                actionButton('clear', 'Clear', icon = icon('eraser')),
-                actionButton("open_chunk", "Insert Chunk", icon = icon('plus'))
-              ),
-              tags$div(
-                class = "col-md-4",
-                h1("Console (Current chunk)"),
-                tags$div(id = "console")
-              ),
-              tags$div(
-                class = "col-md-4",
-                h1("Knirt Completo"),
-                htmlOutput("knitDoc")
-                # Aquí puedes agregar el contenido para la tercera parte
-              )
-            )
-          )
-        )
+library(shinythemes)
+library(shinyWidgets)
+library(shinydashboard)
+
+## Dashboard
+
+ui <- fluidPage(
+  titlePanel("Mi App Shiny"),
+  theme = shinytheme("flatly"),
+  shinydashboard::dashboardPage(
+    header = dashboardHeader(title = "Dashboard Example"),
+    sidebar = dashboardSidebar(
+      sidebarMenu(
+        id = "tabs",
+        menuItem("Help", tabName = "help", icon = icon("question")),
+        menuItem("Themes", tabName = "themes", icon = icon("paint-brush"),
+                 menuSubItem("Application Theme", tabName = "application_theme"),
+                 menuSubItem("Editor Theme", tabName = "editor_theme")),
+        menuItem("Save", icon = icon("th"), tabName = "save",
+                 menuSubItem("Save rmd", tabName = "save_rmd"),
+                 menuSubItem("Save knitr", tabName = "save_knirt")),
+        menuItem("About", icon = icon("bar-chart-o"), tabName = "about",
+                 menuSubItem("Support:", tabName = "support"),
+                 menuSubItem("Source Code", tabName = "source_code"))
       )
+    ),
+    body = dashboardBody(
+      tabItems(
+        tabItem("save_rmd",
+                fluidPage(
+                  titlePanel("Guardar Código RMD"),
+                  h2("Opciones"),
+                  actionButton("theme_button", "Elige un tema"),
+                  selectInput('theme_code', 'Tema editor', choices = themes, selected = 'ambiance'),
+                  downloadButton('save_code', 'Guardar código', icon = icon('save'))
+                )
+        ),
+        tabItem("save_knirt",
+                fluidPage(
+                  titlePanel("Guardar Documento Knitr"),
+                  h2("Archivo Markdown"),
+                  verbatimTextOutput("log"),
+                  aceEditor("rmd", mode = "markdown", value = NULL,
+                            hotkeys = list(
+                              open_chunk = 'Ctrl-Alt-I',
+                              save_code = "Ctrl-S",
+                              help_key = "F1"
+                            ),
+                            autoComplete = "live"),
+                  actionButton("eval", "Run", icon = icon('play')),
+                  actionButton('clear', 'Clear', icon = icon('eraser')),
+                  actionButton("open_chunk", "Insert Chunk", icon = icon('plus'))
+                )
+        ),
+        tabItem("application_theme",
+                fluidPage(
+                  titlePanel("Tema de Aplicación"),
+                  h2("Selecciona un tema"),
+                  actionButton("change_theme", "Cambiar Temática")
+                )
+        ),
+        tabItem("editor_theme",
+                fluidPage(
+                  titlePanel("Tema Editor"),
+                  h2("Selecciona un tema"),
+                  selectInput('theme_code', 'Tema editor', choices = themes, selected = 'ambiance')
+                )
+        )
+      ),
+      bookmarkButton()
     )
   )
+)
